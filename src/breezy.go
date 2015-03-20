da6ea236613ca0c38a1718d3610ebb855db66ff3
Tim <raymondtimothyharris@gmail.com>
2015-03-19 17:28:13 -0700

    More Structs
    Working on breezyActivities

--- src/breezy.go
+++ src/breezy.go
@@ -2,38 +2,46 @@ package main
 
 import (
 	"fmt"
-	//"log"
+	"time"
 	"net/http"
 )
 
 const brImage, brAudio , brVideo = 0, 1, 2
+const brPost = 0
 
-type breezyImage struct{
-	name, filename string
-	fileSize float32
-}
-
-type breezyAudio struct{
-	name, filename string
-	fileSize float32
-}
-
-type breezyVideo struct{
-	name, filename string
-	fileSize float32
-}
-
-func (m breezyImage) String() string{
-	return fmt.Sprintf("%v", m.name)
-}
 type breezyMedia struct{
-	name filename string
+	name, filename string
 	filesSize float32
 	brType int
 }
 
+func (m breezyMedia) String() string{
+	var typeDescription
+	switch m.brType{
+	case brImage:
+		typeDescription = "Image"
+	case brAudio:
+		typeDescription = "Audio"
+	case brVideo:
+		typeDescription = "Video"
+		
+	case default:
+		typeDescription = ""
+	}
+	return fmt.Sprintf("%v, %v", m.name , typeDescription)
+}
+
+type breezyActivity struct{
+	name string
+	activityBody string
+	mediaStructure [5]breezyMedia
+	brType int
+	dateCreated Time
+}
+
+
 func main(){
-	http.Handle("/string", String("I'm all good."))
-	http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
+	//http.Handle("/string", String("I'm all good."))
+	//http.Handle("/struct", &Struct{"Hello", ":", "Gophers!"})
 	http.ListenAndServe("localhost:4000", nil)
 }
