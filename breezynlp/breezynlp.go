83388f33093a6fa9549c056c45c8b3bd83601cad
Tim <raymondtimothyharris@gmail.com>
2015-06-02 22:20:46 -0700

    NLP documentation
    Commented some code for the NLP package. Its getting to a pretty good
    place to start working on some data from breezy blog posts.

--- breezynlp/breezynlp.go
+++ breezynlp/breezynlp.go
@@ -17,35 +17,41 @@ func (brNode BreezyNode) String() string {
 func (brNode *BreezyNode) AddChild(newChild BreezyNeighborObject) bool {
 	// Add child inserts a BreezyNeighberObject in to the Children array
 	// If successful returns true else it returns false
-
-	for i := 0; i < len(brNode.Children); i++ {
+	for i := 0; i < len(brNode.Children); i++ { //loop through the children of the node
 		if brNode.Children[i].Cost == newChild.Cost && brNode.Children[i].Vertex.Payload == newChild.Vertex.Payload {
+			//if child is already present return false
 			return false
 		} else if brNode.Children[i].Cost > newChild.Cost && brNode.Children[i].Vertex.Payload == newChild.Vertex.Payload {
+			// if child is found but new child has lower cost replace with new cost
 			brNode.Children[i].Cost = newChild.Cost
 			return true
 		}
 	}
+	// if child is not found after loop through child add new neighbor
 	brNode.Children = append(brNode.Children, newChild)
 	return true
 }
 
 func (brNode *BreezyNode) removeChild(childToRemove BreezyNode) {
-	foundIndex := 0
+	// removeChild function removes the neighbor connection for a node
+	foundIndex := -1
 	var tempArr []BreezyNeighborObject
-	for i := 0; i < len(brNode.Children); i++ {
+	for i := 0; i < len(brNode.Children); i++ { // loop through children to find the correct child
 		if brNode.Children[i].Vertex.Index == childToRemove.Index && brNode.Children[i].Vertex.Payload == childToRemove.Payload {
+			// If child is found store the index of that correct child
 			foundIndex = i
 		}
 	}
+	// make temp array of all items after found index and shorten orginal array to all items before index
 	tempArr = brNode.Children[foundIndex+1 : len(brNode.Children)]
 	brNode.Children = brNode.Children[0 : foundIndex-1]
-	for j := 0; j < len(tempArr); j++ {
+	for j := 0; j < len(tempArr); j++ { // loop through tempArr to add back elements after the index
 		brNode.Children = append(brNode.Children, tempArr[j])
 	}
 }
 
 type BreezyNeighborObject struct {
+	// BreezyNeighborObject stores the neccessary data a connection between two nodes
 	Vertex BreezyNode // Connecting neighbor node
 	Cost   int        // Cost to go to new vertex from original
 }
@@ -55,6 +61,7 @@ func (brNeighbor BreezyNeighborObject) String() string {
 }
 
 type BreezyGraph struct {
+	// BreezyGraph stores the neccessary data to make a graph
 	BreezyADJList     []BreezyNode // Array of vertices within the graph
 	NumberOfVerticies int          // number of vertices in the graph
 	NumberOfEdges     int          // number of edges in the graph
@@ -127,6 +134,7 @@ func (brGraph *BreezyGraph) RemoveVertex(vertexToRemove BreezyNode) bool {
 		for i := 0; i < len(tempArr); i++ {
 			brGraph.BreezyADJList = append(brGraph.BreezyADJList, tempArr[i])
 		}
+		brGraph.NumberOfVerticies--
 		return true
 	} else {
 		return false
@@ -134,30 +142,41 @@ func (brGraph *BreezyGraph) RemoveVertex(vertexToRemove BreezyNode) bool {
 }
 
 func (brGraph *BreezyGraph) RemoveEdge(fromVertex BreezyNode, andVertex BreezyNode) {
+	firstHalf, secondHalf := false, false
 	for i := 0; i < len(brGraph.BreezyADJList); i++ {
 		if brGraph.BreezyADJList[i].Index == fromVertex.Index && brGraph.BreezyADJList[i].Payload == fromVertex.Payload {
 			// remove child from fromVertex
 			fromVertex.removeChild(andVertex)
+			firstHalf = true
 		}
 		if brGraph.BreezyADJList[i].Index == andVertex.Index && brGraph.BreezyADJList[i].Payload == andVertex.Payload {
 			// remove child from andvVrtex
 			andVertex.removeChild(fromVertex)
+			secondHalf = true
 		}
+	}
+	if firstHalf == true && secondHalf == true {
+		brGraph.NumberOfEdges--
 	}
 }
 
+// Queue
+
 type BreezyQueueNode struct {
-	Index   int
-	Payload string
-	Next    *BreezyQueueNode
+	// BreezyQueueNode stores the node data for a queue
+	Index   int              // Index from BreezyNode stored in queue
+	Payload string           // Payload from BreezyNode stored in a queue
+	Next    *BreezyQueueNode // Pointer to the queue node connected to this node
 }
 type BreezyQueue struct {
-	First  *BreezyQueueNode
-	Last   *BreezyQueueNode
-	Length int
+	// BreezyQueue stores the data for a queue data structure
+	First  *BreezyQueueNode // Pointer to the first element in the queue
+	Last   *BreezyQueueNode // Pointer to the last element in the queue
+	Length int              // Value for the length of the queue
 }
 
 func (brQueue *BreezyQueue) enqueue(newNode BreezyQueueNode) {
+	// Enqueue inserts a new queue node at he end of the queue
 	if brQueue.First == nil {
 		brQueue.First = &newNode
 		brQueue.Last = brQueue.First
@@ -169,6 +188,7 @@ func (brQueue *BreezyQueue) enqueue(newNode BreezyQueueNode) {
 }
 
 func (brQueue *BreezyQueue) dequeue() BreezyQueueNode {
+	// Dequeue pops off the first element in the queue
 	if brQueue.First != nil {
 		returnNode := brQueue.First
 		brQueue.First = brQueue.First.Next
