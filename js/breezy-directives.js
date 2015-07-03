de3ae045c03ffb14b3e6afaf29c6ee92afdcedb0
Tim <raymondtimothyharris@gmail.com>
2015-07-02 22:04:45 -0700

    Moving code in to more specialized files. Started adding code to breezy-services.

---
+++ js/breezy-directives.js
@@ -0,0 +1,184 @@
+breezy.directive('breezyActivity', function(){
+	return{
+		restrict:'E',
+		controller: function($scope, $element){
+
+		},
+		template: function(){
+			return "<div>Activity</div>"
+		}
+	}
+});
+
+breezy.directive('droppable', function(){
+	//droppable directive enables an element to have the droppable behavior
+	return{
+		restrict:'A',
+		scope:{
+			progress:'='
+		},
+		controller: function($scope, $element, $files){
+			$scope.progress = $files.currentProgress;
+
+			$element.on('dragover', function(evt){
+				//DragOver State
+				evt.dataTransfer.dropEffect ="all";
+				if(evt.preventDefault) evt.preventDefault();
+			});
+			$element.on('dragenter', function(evt){
+				//DragEnter State
+			});
+			$element.on('dragleave', function(evt){
+				//DragLeave State
+			});
+			$element.on('drop', function(evt){
+				//Drop State
+				if(evt.preventDefault) evt.preventDefault();
+				if(evt.dataTransfer.files.length >0){
+					var filesList = evt.dataTransfer.files;
+					$files.upload(filesList);
+
+				}
+				else{
+					$element.append(evt.target.getData("text/html"));
+				}
+			});
+			$scope.$on('started', function(){
+				//console.log('upload started');
+			});
+			$scope.$on('uploadProgress', function(evt, progress){
+				//console.log(progress);
+			});
+			$scope.$on('uploadComplete', function(evt, file){
+				$scope.$emit('sortFile', file);
+			});
+			$scope.$on('uploadCompleted', function(){
+				$scope.$emit('addElements');
+			});
+		}
+	}
+});
+
+breezy.directive('draggable', function(){
+	//draggle directive enables an element to have a draggable behavior
+	return{
+		restrict:'A',
+		scope:{
+			sourceElement:'='
+		},
+		controller: function($scope, $element){
+			$scope.sourceElement = $element[0];
+			$scope.sourceElement.draggable = true;
+
+			$element.on('dragstart', function(evt){
+				evt.dataTransfer.effectAllowed ="all";
+				evt.dataTransfer.setData('text/html', this.innerHTML);
+			});
+			$element.on('dragend', function(evt){
+
+			});
+		}
+	}
+});
+breezy.directive("breezyNavigation", function(){
+	return{
+		restrict:'E',
+		controller: function($scope, $http, $window){
+			$scope.openBlog = function(){
+				//Open the users blog in a separate page/tab
+				$window.open("/")
+			}
+			$scope.gotoPage = function(option){
+				console.log(option)
+				switch(option){
+					case "settings":
+					$window.location.href="/settings"
+					break;
+				case "media":
+					$window.location.href="/medialist"
+					break;
+				case "posts":
+					$window.location.href="/postlist"
+					break;
+				case "new post":
+					$window.location.href="/edit"
+					break;
+				case "home":
+					$window.location.href="/dashboard"
+					break;
+				default:
+					console.log("No locaiton to go to.")
+					break;
+	
+				}
+			}
+		},
+		templateUrl : 'views/dashboardnavigation.html'		
+	}
+});
+breezy.directive('contenteditable', ['$sce', function($sce) {
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: '?ngModel', // get a hold of NgModelController
+    link: function(scope, element, attrs, ngModel) {
+      if (!ngModel) return; // do nothing if no ng-model
+
+      // Specify how UI should be updated
+      ngModel.$render = function() {
+        element.html($sce.getTrustedHtml(ngModel.$viewValue || ''));
+      };
+
+      // Listen for change events to enable binding
+      element.on('blur keyup change', function() {
+        scope.$evalAsync(read);
+		if(!scope.contentDirty){
+			scope.contentDirty = true
+		}
+      });
+      read(); // initialize
+
+      // Write data to the model
+      function read() {
+        var html = element.html();
+        // When we clear the content editable the browser leaves a <br> behind
+        // If strip-br attribute is provided then we strip this out
+        if ( attrs.stripBr && html == '<br>' ) {
+          html = '';
+        }
+
+        ngModel.$setViewValue(html);
+      }
+    }
+  };
+}]);
+
+breezy.directive('infiniteScroll', function() {
+	return {
+		restrict: 'A',
+		link:function($scope, elemen, attr){
+			var container = elemen[0]
+	  		elemen.bind("onScroll", function() {
+				if(container.scrollTop + container.offsetHeight >= container.scrollHeight - 40){
+					console.log("add more")
+					$scope.loadMorePosts()
+					$scope.$apply()
+				}
+			})
+		},
+		controller: function($scope, $element, $window){
+			var elm = $element[0]
+				//console.log(elm)
+			angular.element($window).bind("scroll", function() {
+				var scrollWindow = angular.element($window)
+					//console.log(elm.scrollHeight - $window.scrollY)
+				if(elm.scrollHeight - $window.scrollY < elm.clientHeight + 20){
+					//console.log("add more")
+					$scope.loadMorePosts()
+					$scope.$apply()
+				}
+			})
+
+		}
+	}
+})
+
---
+++ js/breezy-services.js
@@ -0,0 +1,140 @@
+breezy.service('$dateformat', function($rootScope){
+	this.formatDate = function(dateString){
+		var dateHalf = dateString.split("T")
+		var dateParts = dateHalf[0].split("-")
+		//console.log(dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0])
+		return dateParts[2] + "/" + dateParts[1] + "/" + dateParts[0]
+	}
+});
+
+breezy.service('$datastorage', function($rootScope, $http){
+	//Service that interacts with the web backened to get and post data to the server
+	this.GetAllPosts = function(){//Function returns all posts stored in the database
+		$http.get("/get_all_posts").success(function(data){
+			//console.log(data)
+			return data
+		})
+
+	}
+	this.GetAllMedia = function(mediaFlag){//This function gets all media in the database and can be culled by type
+		$http.get("/getallmedia/"+mediaFlag).success(function(data){
+			return data
+		})
+	}
+	this.GetPostsBetween = function(lower,upper){
+		$http.get("/get_posts/between/"+lower+"-"+upper).success(function(data){
+			return data
+		})	
+	}
+	this.GetPosts = function(option, filter){
+		switch(option){
+			case "created":
+				$http.get("/get_posts/created/"+filter).success(function(data){
+					return data
+				});
+				break
+			case "creator":
+				$http.get("/get_posts/creator/"+filter).success(function(data){
+					return data
+				});
+				break
+			case "single":
+				$http.get("/get_posts/single/"+filter).success(function(data){
+					return data
+				});
+				break
+		}
+	}
+	this.GetMedia = function(option, filter){
+		switch(option){
+			case "single":
+				$http.get("/get_media/single/"+filter).success(function(data){
+					return data
+				});
+				break
+			case "multi":
+				$http.post("/get_media/multi/", filter).success(function(data){
+					return data
+				});
+				break
+			case "added":
+				$http.get("/get_media/added/"+filter).success(function(data){
+					return data
+				});
+				break
+
+		}
+	}
+});
+
+breezy.service('$files', function($rootScope,$http){
+	var currentProgress;
+	var uploadSize;
+	var currentFileSize;
+	this.upload = function(fileList){
+		console.log(fileList)
+		currentProgress= 0;
+		uploadSize=0;
+		for(var i = 0; i < fileList.length; i++){
+			uploadSize +=fileList[i].size;
+		}
+		//console.log(uploadSize);
+		for(var i = 0; i < fileList.length; i++){
+			var xmlHttpReq = new XMLHttpRequest();
+			currentFileSize = fileList[i].size;
+			var formdata = new FormData();
+			formdata.append("file", fileList[i]);
+			formdata.append("filetype", fileList[i].type);
+			xmlHttpReq.open("POST", "/uploadfile");
+			xmlHttpReq.upload.addEventListener("progress", this.uploadProgress, false);
+			xmlHttpReq.addEventListener("load", this.uploadComplete(xmlHttpReq, fileList[i]), false);
+			xmlHttpReq.send(formdata);
+			/*
+			xmlHttpReq.setRequestHeader('X_FILE_NAME', fileList[i].name);
+			xmlHttpReq.setRequestHeader('X_FILE_SIZE', fileList[i].size);
+			xmlHttpReq.setRequestHeader('X-Requested-With', true);
+			*/
+	//		xmlHttpReq.setRequestHeader('Content-Type', fileList[i].type);
+		//	xmlHttpReq.setRequestHeader('Content-Type', 'multipart/form-data');
+			console.log(fileList[i].type);
+		/*	xmlHttpReq.upload.addEventListener("progress", this.uploadProgress, false);
+			xmlHttpReq.upload.addEventListener("loadstart", this.uploadStart, false);
+			//xmlHttpReq.upload.addEventListener("loadend", this.uploadEnd(xmlHttpReq.upload, fileList[i]), false);
+			xmlHttpReq.addEventListener("load", this.uploadComplete(xmlHttpReq, fileList[i]), false);
+			//xmlHttpReq.addEventListener("error", this.uploadFailed, false);
+			//xhr.addEventListener("abort", uploadCanceled, false)
+		*/
+		//	xmlHttpReq.send(fileList[i]);
+			if(i == fileList.length-1){
+				this.allFiles(fileList);
+			}
+		}
+	}
+	var getUploadSize = function(){
+		return uploadSize;
+	}
+	this.uploadStart = function(){
+	//	$rootScope.$broadcast('started');
+	}
+	this.uploadProgress = function(evt){
+
+		currentProgress += Math.round(evt.loaded * 100 / uploadSize);
+
+	//	$rootScope.$broadcast('uploadProgress', currentProgress);
+	}
+	this.uploadComplete = function(req, file){
+		console.log(req);
+		$rootScope.$broadcast('uploadComplete',file);
+	}
+	this.uploadFailed = function(){
+
+	}
+	this.uploadEnd = function(req, file){
+		console.log('fi');
+		//$rootScope.$broadcast('uploadComplete', file);
+	 }
+	this.allFiles = function(fileList){
+		$rootScope.$broadcast('uploadCompleted',fileList);
+	}
+
+});
